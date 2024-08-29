99bfe1393e347dcd1005396eae1391fcb84cd022
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2024-08-30 00:31:20 +0300

    implement register feature

---
+++ app/Http/Requests/Website/Auth/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Website\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+
+    public function registerUser(): array
+    {
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        return [
+            'user' => $user,
+            'token' => $user->createToken('api_token')->plainTextToken,
+        ];
+    }
+}
