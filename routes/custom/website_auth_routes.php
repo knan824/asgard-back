86effce77338c040a20cf1ff2fed22d117163184
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2024-09-07 04:04:49 +0300

    implement login feature

--- routes/custom/website_auth_routes.php
+++ routes/custom/website_auth_routes.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Website\Auth\LoginController;
 use App\Http\Controllers\Website\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [RegisterController::class, 'store'])->name('register');
+Route::post('login', [LoginController::class, 'store'])->name('login');
