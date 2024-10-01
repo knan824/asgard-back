a8232ff6fded69ef1205f48fcb222406a6050952
Anan <youssef.i.anan@hotmail.com>
2024-10-01 16:38:42 +0300

    Users and wishlists translations

---
+++ lang/en/Wishlists.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'store' => 'Wishlist item added successfully',
+    'delete' => 'Wishlist item removed successfully',
+    'already' => 'Item is already in your wishlist',
+];
